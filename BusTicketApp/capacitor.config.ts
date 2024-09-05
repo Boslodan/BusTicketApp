1826be137f841385e14fdfd40519b54a370513bc
MilosFilipovic <filipovicmilos001@gmail.com>
2024-09-05 11:12:56 +0200

    Kreiranje Ionic projekta

---
+++ BusTicketApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BusTicketApp',
+  webDir: 'dist'
+};
+
+export default config;
