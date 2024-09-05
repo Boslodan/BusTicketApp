3c80a1203255e2828807001140d916280d95718d
Boslodan <sjankovic01@gmail.com>
2024-09-05 22:52:36 +0200

    Home-SearchBar,BurgerMeni

--- BusTicketApp/src/pages/Home.tsx
+++ BusTicketApp/src/pages/Home.tsx
@@ -1,61 +1,102 @@
 import React, { useState } from 'react';
-import {IonPage,IonHeader,IonToolbar,IonTitle,IonContent,IonList,IonItem,IonLabel,IonButton,IonGrid,IonRow,IonCol,IonCard,IonCardHeader,IonCardTitle,IonCardContent,} from '@ionic/react';
+import {IonPage,IonHeader,IonToolbar,IonTitle,IonContent,IonList,IonItem,IonLabel,IonButton,IonGrid,IonRow,IonCol,IonCard,IonCardHeader,IonCardTitle,IonCardContent,IonMenu,IonMenuButton,IonButtons,IonSearchbar,} from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+//import './Home.css';
 
 const Home: React.FC = () => {
+    const [tickets, setTickets] = useState([
+        { id: 1, route: 'Beograd -> Novi sad', price: 1200, date:'24.10.2024' },
+        { id: 2, route: 'Beograd -> Nis', price: 1700, date:'12.10.2024' },
+        { id: 3, route: 'Novi sad -> Subotica', price: 900, date:'03.10.2024' },
+        { id: 4, route: 'Beograd -> Budimpesta', price: 3000, date:'22.12.2024' }
+      ]);
 
-  const [tickets, setTickets] = useState([
-    { id: 1, route: 'Beograd -> Novi sad', price: 1200, date:'24-10-2024' },
-    { id: 2, route: 'Beograd -> Nis', price: 1700, date:'12-10-2024' },
-    { id: 3, route: 'Novi sad -> Subotica', price: 900, date:'03-10-2024' },
-    { id: 4, route: 'Beograd -> Budimpesta', price: 3000, date:'22-12-2024' }
-  ]);
+      
+  const history = useHistory();
+  const [searchText, setSearchText] = useState('');
+
 
   const handleBuyTicket = (ticketId: number) => {
     alert(`Ticket ${ticketId} purchased successfully!`);
   };
 
+  const handleNavigation = (path: string) => {
+    history.push(path);
+  };
+
+  const filteredTickets = tickets.filter((ticket) =>
+    ticket.route.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar style={{ '--background': '#1F2833', '--color': '#66FCF1' }}>
-          <IonTitle>BusTickets</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding" style={{ '--background': '#66FCF1' }}>
-        <IonGrid>
-          <IonRow>
-            <IonCol size="12">
-              <IonCard>
-                <IonCardHeader>
-                  <IonCardTitle>Dostupne linije</IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent>
-                  <IonList>
-                    {tickets.map((ticket) => (
-                      <IonItem key={ticket.id}>
-                        <IonLabel>
-                          <h2>{ticket.route}</h2>
-                          <p>Cena: {ticket.price} RSD</p>
-                          <p>Datum: {ticket.date}</p>
-                        </IonLabel>
-                        <IonButton
-                          slot="end"
-                          color="primary"
-                          onClick={() => handleBuyTicket(ticket.id)}
-                        >
-                          Buy
-                        </IonButton>
-                      </IonItem>
-                    ))}
-                  </IonList>
-                </IonCardContent>
-              </IonCard>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </IonContent>
-    </IonPage>
+    <>
+      <IonMenu side="start" contentId="main-content">
+        <IonContent>
+          <IonList>
+            <IonItem button onClick={() => handleNavigation('/myTickets')}>
+              <IonLabel>Moje karte</IonLabel>
+            </IonItem>
+            <IonItem button onClick={() => handleNavigation('/myAccount')}>
+              <IonLabel>Moj nalog</IonLabel>
+            </IonItem>
+            <IonItem button onClick={() => handleNavigation('/login')}>
+              <IonLabel>Odjava</IonLabel>
+            </IonItem>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar style={{ '--background': '#1F2833', '--color': '#66FCF1' }}>
+            <IonButtons slot="start">
+              <IonMenuButton />
+            </IonButtons>
+            <IonTitle>BusTickets</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding" style={{ '--background': '#66FCF1' }}>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12">
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>Dostupne linije</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                  <IonSearchbar
+                      value={searchText}
+                      onIonChange={(e) => setSearchText(e.detail.value!)}
+                      placeholder="Pretraži linije"
+                    />
+                    <IonList>
+                      {filteredTickets.map((ticket) => (
+                        <IonItem key={ticket.id}>
+                          <IonLabel>
+                            <h2>{ticket.route}</h2>
+                            <p>Cena: {ticket.price} Rsd</p>
+                            <p>Datum: {ticket.date}</p>
+                          </IonLabel>
+                          <IonButton
+                            slot="end"
+                            color="primary"
+                            onClick={() => handleBuyTicket(ticket.id)}
+                          >
+                            Kupi
+                          </IonButton>
+                        </IonItem>
+                      ))}
+                    </IonList>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonContent>
+      </IonPage>
+    </>
   );
 };
 
 export default Home;
+
